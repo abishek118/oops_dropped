cd42b0fa59a1b19b503d9f5e4915d6bc3fb4957a
abishek118 <abishek181@gmail.com>
2026-07-16 11:17:12 +0530

    Configure MainActivity to show programmatically on top of lock screen and wake screen

--- android/app/src/main/kotlin/com/example/oops_dropped/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/oops_dropped/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.oops_dropped
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setShowWhenLocked(true)
+        setTurnScreenOn(true)
+    }
+}
